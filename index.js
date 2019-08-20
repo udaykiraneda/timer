a8fdca29ac74bfebe6f434d586d53a245e0bac97
uday kiran <kiran4frndz@gmail.com>
2019-08-20 18:10:09 +0530

    write code to print numbers with delay of 1 sec

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log('This is a timer application');
+var i;
+for (i = 1; i <= 60; i++){
+    print(i);
+}
 
+function print(i) {
+    setTimeout(function(){
+        console.log(i)
+    },i*1000);
+}
